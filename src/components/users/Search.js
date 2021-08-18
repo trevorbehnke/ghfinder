7c16a651586930ac345d01ada2c036ce82e49905
trevor <me@trevorbehnke.com>
2021-08-17 22:34:38 -0700

    refactor & +clear button

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,17 +1,25 @@
 import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class Search extends Component {
   state = {
     text: "",
   };
 
+  static propTypes = {
+    searchUsers: PropTypes.func.isRequired,
+    clearUsers: PropTypes.func.isRequired,
+    showClear: PropTypes.bool.isRequired,
+  };
+
   onSubmit = (e) => {
     e.preventDefault();
-    console.log(this.state.text);
+    this.props.searchUsers(this.state.text);
   };
 
   onChange = (e) => this.setState({ [e.target.name]: e.target.value });
   render() {
+    const { showClear, clearUsers } = this.props;
     return (
       <div>
         <form onSubmit={this.onSubmit} className="form">
@@ -28,6 +36,11 @@ class Search extends Component {
             className="btn btn-dark btn-block"
           ></input>
         </form>
+        {showClear && (
+          <button className="btn btn-light btn-block" onClick={clearUsers}>
+            Clear
+          </button>
+        )}
       </div>
     );
   }
